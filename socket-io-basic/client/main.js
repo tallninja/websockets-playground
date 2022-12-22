c276ceaac0e382de9654260b53c4af29807821ec
Ernest Wambua <ernestwambua2@gmail.com>
2022-12-22 12:24:34 +0300

    Can now shnchronize movement

--- socket-io-basic/client/main.js
+++ socket-io-basic/client/main.js
@@ -7,33 +7,53 @@ const chats = document.querySelector('#chats');
 const usernames = ['nick', 'corey', 'desmond', 'jack', 'jim', 'bob'];
 
 sendButton.onclick = () => {
-  const message = chatInput.value;
-  socket.emit('message', message);
+	const message = chatInput.value;
+	socket.emit('message', message);
 };
 
+const field = document.querySelector('#field');
+const user1 = document.querySelector('#user1');
+
+field.addEventListener('mousemove', (e) => {
+	const position = {
+		x: e.clientX,
+		y: e.clientY,
+	};
+	user1.style.left = position.x + 'px';
+	user1.style.top = position.y + 'px';
+	// console.log(position);
+	socket.emit('cursorMove', position);
+});
+
 socket.on('connect', () => {
-  if (socket.connected) {
-    console.log('Connected to server !');
-    socket.emit(
-      'set username',
-      usernames[Math.floor(Math.random() * usernames.length)]
-    );
-  } else {
-    console.log('Failed to connect to server !');
-  }
+	if (socket.connected) {
+		console.log('Connected to server !');
+		socket.emit(
+			'set username',
+			usernames[Math.floor(Math.random() * usernames.length)]
+		);
+	} else {
+		console.log('Failed to connect to server !');
+	}
 });
 
 socket.on('disconnect', () => {
-  if (socket.disconnected) {
-    console.log('Disconnected from server !');
-  } else {
-    console.log('Failed to disconnect from server !');
-  }
+	if (socket.disconnected) {
+		console.log('Disconnected from server !');
+	} else {
+		console.log('Failed to disconnect from server !');
+	}
 });
 
 socket.on('message', (data) => {
-  console.log(`${data.user}: ${data.message}`);
-  const chat = document.createElement('li');
-  chat.innerText = `${data.user}: ${data.message}`;
-  chats.appendChild(chat);
+	console.log(`${data.user}: ${data.message}`);
+	const chat = document.createElement('li');
+	chat.innerText = `${data.user}: ${data.message}`;
+	chats.appendChild(chat);
+});
+
+socket.on('cursorMove', (position) => {
+	console.log(position);
+	user1.style.left = position.x + 'px';
+	user1.style.top = position.y + 'px';
 });
--- socket-io-basic/server/index.js
+++ socket-io-basic/server/index.js
@@ -6,44 +6,52 @@ const app = require('./src/app');
 const httpserver = createServer(app);
 
 const io = new Server(httpserver, {
-  cors: {
-    origin: '*',
-    credentials: true,
-  },
+	cors: {
+		origin: '*',
+		credentials: true,
+	},
 });
 
-const users = [];
+let users = [];
 
 io.on('connection', (socket) => {
-  console.log('client connected');
-  socket.on('disconnect', () => {
-    console.log('client disconnected');
-    users.filter((user) => user != socket.username);
-  });
+	console.log('client connected');
+	socket.on('disconnect', () => {
+		console.log('client disconnected');
+		users.filter((user) => user != socket.username);
+	});
 
-  socket.on('set username', (username) => {
-    socket.username = username;
-    users.push(socket.username);
-    console.log(users);
-  });
+	socket.on('set username', (username) => {
+		socket.username = username;
+		users.push(socket.username);
+		console.log(users);
+	});
 
-  socket.on('hello', (data) => {
-    console.log(`client: ${data}`);
-    socket.emit('hello', data);
-  });
+	socket.on('hello', (data) => {
+		console.log(`client: ${data}`);
+		socket.emit('hello', data);
+	});
 
-  socket.on('message', (message) => {
-    io.emit('message', { message: message, user: socket.username });
-  });
+	socket.on('message', (message) => {
+		io.emit('message', { message: message, user: socket.username });
+	});
+
+	socket.on('cursorMove', (position) => {
+		socket.broadcast.emit('cursorMove', position); // send to all clients apart from the sender
+	});
+
+	socket.on('disconnect', () => {
+		users = users.filter((user) => user !== socket.username);
+	});
 });
 
 const PORT = process.env.PORT || 5000;
 httpserver.listen(PORT);
 
 httpserver.on('error', (err) => {
-  console.log('Error:', err);
+	console.log('Error:', err);
 });
 
 httpserver.on('listening', () => {
-  console.log(`Server listening on PORT: ${PORT}`);
+	console.log(`Server listening on PORT: ${PORT}`);
 });
